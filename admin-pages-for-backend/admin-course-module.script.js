11c728036e2a72395f1e7ae116a4eb63de2aea94
Pyae Hein <pyaehein.sky@gmail.com>
2023-09-01 23:49:13 +0630

    course module for admin

---
+++ admin-pages-for-backend/admin-course-module.script.js
@@ -0,0 +1,7 @@
+const add_course_module = document.querySelector(".add-course-module");
+
+const delete_course_module = document.querySelector(".delete-course-module");
+
+const delete_course_module_material = document.querySelector(
+	".delete-course-module-material"
+);
