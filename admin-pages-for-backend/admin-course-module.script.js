9fa0bcee0e4be2b12b7de7daf7d0472699ce50f6
Pyae Hein <pyaehein.sky@gmail.com>
2023-09-06 23:51:49 +0630

    styling

--- admin-pages-for-backend/admin-course-module.script.js
+++ admin-pages-for-backend/admin-course-module.script.js
@@ -32,13 +32,49 @@ btnCancle.addEventListener("click", (event) => {
 	module_material_creation_form.classList.toggle("hidden");
 });
 
-
 // in uni
-const add_task = document.querySelector(".add-task")
-const addMaterial = document.querySelector(".add-material")
-
+const add_task = document.querySelector(".add-task");
+const addMaterial = document.querySelector(".add-material");
+const addAssignment = document.querySelector(".add-assignment");
+const addQuiz = document.querySelector(".add-quiz");
 
 add_course_module_material.addEventListener("click", () => {
-	add_task.classList.toggle("hidden")
+	add_task.classList.toggle("hidden");
 });
-addMaterial.addEventListener("click", () => module_material_creation_form.classList.toggle("hidden"))
+addMaterial.addEventListener("click", () =>
+	module_material_creation_form.classList.toggle("hidden")
+);
+
+// FOR ASSIGNMENT
+
+const assignmentForm = document.querySelector(".assignment-wrapper");
+
+// create task
+//const btnCreateAssignment = document.querySelector(".btn-create-assignment");
+
+// form close btn
+const closeBtn = document.querySelector(".close-button");
+
+addAssignment.addEventListener("click", () => {
+	console.log("create");
+	assignmentForm.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+	assignmentForm.style.display = "none";
+});
+
+// FOR QUIZ
+// 	MAIN-QUIZ-WRAPPER
+
+const mainQuizForm = document.querySelector(".main-quiz-wrapper");
+const btnQuiz = document.querySelector(".btn-quiz");
+const closeBtnMainQuiz = document.querySelector(".close-button-main-quiz");
+addQuiz.addEventListener("click", () => {
+	console.log("create");
+	mainQuizForm.style.display = "block";
+});
+
+closeBtnMainQuiz.addEventListener("click", () => {
+	mainQuizForm.style.display = "none";
+});
